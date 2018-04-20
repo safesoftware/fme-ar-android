3d1e86bf3bed7b038fe82808cc053eaabe5a732b
Angus Lau <angus.lau@safe.com>
2018-04-20 14:11:02 -0700

    FME AR: Initial project definition.

---
+++ FMEAR/app/src/main/java/com/safe/fmear/ARActivity.java
@@ -0,0 +1,13 @@
+package com.safe.fmear;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ARActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ar);
+    }
+}
