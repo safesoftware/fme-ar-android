5b83e95ac7af198e8324ce98d63a5457d937fdb3
Angus Lau <angus.lau@safe.com>
2020-06-18 03:45:16 -0700

    Added a splash screen with dark mode support.

---
+++ FMEAR/app/src/main/java/com/safe/fmear/MainActivity.kt
@@ -0,0 +1,4 @@
+package com.safe.fmear
+
+class MainActivity {
+}
---
+++ FMEAR/app/src/main/java/com/safe/fmear/SplashScreenActivity.kt
@@ -0,0 +1,13 @@
+package com.safe.fmear
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity)
+//        startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+//        finish();
+    }
+}
